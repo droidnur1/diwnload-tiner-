0a15418e4e2309f0f4b2b711a873da02d76e609d
Nur Islam Gazi <85954697+droidnur1@users.noreply.github.com>
2024-08-06 20:27:33 +0530

    Update page split script.js

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-
-const downloadBtn = document.querySelector(".button");
-const fileLink = "";
-
-const initTimer = () => {
-    if(downloadBtn.classList.contains("disable-timer")) {
-        return location.href = fileLink;
-    }
-    let timer = downloadBtn.dataset.timer;
-    downloadBtn.classList.add("timer");
-    downloadBtn.innerHTML = `Your download will begin in <b> (${timer}) </b> seconds`;
-    const initCounter = setInterval(() => {
-        if(timer > 0) {
-            timer--;
-            return downloadBtn.innerHTML = `Your download will begin in <b> (${timer})</b>  seconds`;
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+    function checkChildren(nodes, elemId) {
+        for (i = 0; i < nodes.length; i++) {
+            if (nodes[i].id == elemId) {
+                return nodes[i];
+            } else {
+                return checkChildren(nodes[i].children, elemId);
+            }
         }
-        clearInterval(initCounter);
-        location.href = fileLink;
-        downloadBtn.innerText = "Your file is downloading...";
-        setTimeout(() => {
-            downloadBtn.classList.replace("timer", "disable-timer");
-           
-                                    
-        }, 3000);
-    }, 1000);
-}
+    }
 
-downloadBtn.addEventListener("click", initTimer);
+    function isNumeric(value) {
+        var type = typeof value;
+        return (type === 'number' || type === 'string') && !Number.isNaN(value - Number.parseFloat(value));
+    }
+    var nodes = document.querySelector('div.postBody').children;
+    var splitdong = checkChildren(nodes, 'postSplit').innerHTML;
+    var content = splitdong.split('<!--nextpage-->');
+    var url = window.location.href;
+    var url = url.split('?page=');
+    var no = url[1] + '&m=4';
+    var no = no.split('m');
+    var no = no[0];
+    var no = no.replace('&', '');
+    var url = url[0];
+    var i = 1;
+    if (!isNumeric(no)) {
+        var no = 1;
+    }
+    document.getElementById('postSplit').innerHTML = content[no - 1];
+    if (content.length > 1) {
+        document.getElementById('postSplit').innerHTML += "<div class='blogPg' id='postPager'><span class='page current'>Pages :</span></div>";
+    }
+    if (no > 1) {
+        document.getElementById('postPager').innerHTML += "<!-- <a href='" + url + "?page=" + (no - 1) + "' title='Previous Page'>Prev</a> -->";
+    }
+    content.forEach(function (item) {
+        if (no == i) {
+            document.getElementById('postPager').innerHTML += "<span class='current'>" + i + "</span>";
+        } else {
+            document.getElementById('postPager').innerHTML += "<a href='" + url + "?page=" + i + "'>" + i + "</a>";
+        }
+        i++;
+    });
+    if (content.length > no) {
+        var nn = parseInt(no) + 1;
+        document.getElementById('postPager').innerHTML += "<a href='" + url + "?page=" + nn + "'>Next</a>";
+    }
+}); </script>

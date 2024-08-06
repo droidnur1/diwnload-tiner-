9b4167a9d758e944660ed46b65de989132629299
Nur Islam Gazi <85954697+droidnur1@users.noreply.github.com>
2024-08-06 20:03:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<script>
+const downloadBtn = document.querySelector(".button");
+const fileLink = "";
+
+const initTimer = () => {
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
+    let timer = downloadBtn.dataset.timer;
+    downloadBtn.classList.add("timer");
+    downloadBtn.innerHTML = `Your download will begin in <b> (${timer}) </b> seconds`;
+    const initCounter = setInterval(() => {
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `Your download will begin in <b> (${timer})</b>  seconds`;
+        }
+        clearInterval(initCounter);
+        location.href = fileLink;
+        downloadBtn.innerText = "Your file is downloading...";
+        setTimeout(() => {
+            downloadBtn.classList.replace("timer", "disable-timer");
+           
+                                    
+        }, 3000);
+    }, 1000);
+}
+
+downloadBtn.addEventListener("click", initTimer);</script>
